0cdfe9654d849de6efc632293a1a80e256fa4fcf
magegihk <58164998+magegihk@users.noreply.github.com>
2019-12-14 09:50:45 +0800

    delete what is not needed in spriteinfo

--- HollowKnight.SpritePacker/Classes/SpriteInfo.cs
+++ HollowKnight.SpritePacker/Classes/SpriteInfo.cs
@@ -9,10 +9,6 @@ namespace HollowKnight.SpritePacker
         public List<int> sid;
         public List<int> sx;
         public List<int> sy;
-        public List<int> swidth;
-        public List<int> sheight;
-        public List<int> satlaswidth;
-        public List<int> satlasheight;
 
         public List<string> scollectionname;
         public List<string> spath;
@@ -21,7 +17,7 @@ namespace HollowKnight.SpritePacker
 
         public Sprite this[int i]
         {
-            get { return new Sprite(sid[i], sx[i], sy[i], swidth[i], sheight[i], satlaswidth[i], satlasheight[i], scollectionname[i], spath[i], sfilpped[i]); }
+            get { return new Sprite(sid[i], sx[i], sy[i], scollectionname[i], spath[i], sfilpped[i]); }
         }
 
         public int Length { get => sid.Count; }
@@ -31,10 +27,6 @@ namespace HollowKnight.SpritePacker
             sid = new List<int>();
             sx = new List<int>();
             sy = new List<int>();
-            swidth = new List<int>();
-            sheight = new List<int>();
-            satlaswidth = new List<int>();
-            satlasheight = new List<int>();
 
             scollectionname = new List<string>();
             spath = new List<string>();
@@ -42,15 +34,11 @@ namespace HollowKnight.SpritePacker
             sfilpped = new List<bool>();
         }
 
-        public void Add(int _sid, int _sx, int _sy, int _swidth, int _height, int _satlaswidth, int _satlasheight, string _scollectionname, string _spath, bool _sfilpped)
+        public void Add(int _sid, int _sx, int _sy, string _scollectionname, string _spath, bool _sfilpped)
         {
             sid.Add(_sid);
             sx.Add(_sx);
             sy.Add(_sy);
-            swidth.Add(_swidth);
-            sheight.Add(_height);
-            satlaswidth.Add(_satlaswidth);
-            satlasheight.Add(_satlasheight);
             scollectionname.Add(_scollectionname);
             spath.Add(_spath);
             sfilpped.Add(_sfilpped);
